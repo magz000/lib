441511127bbd3a778d380273e509a0aaa7738d80
crlflrs23 <carlo29092@gmail.com>
2018-01-05 15:49:34 +0800

    late commit

---
+++ sql/1-5-2018.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.6
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 05, 2018 at 08:42 AM
+-- Server version: 10.1.29-MariaDB
+-- PHP Version: 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lib`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `avatars`
+--
+-- Error reading structure for table lib.avatars: #1932 - Table 'lib.avatars' doesn't exist in engine
+-- Error reading data for table lib.avatars: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lib`.`avatars`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groupchats`
+--
+-- Error reading structure for table lib.groupchats: #1932 - Table 'lib.groupchats' doesn't exist in engine
+-- Error reading data for table lib.groupchats: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lib`.`groupchats`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table lib.migrations: #1932 - Table 'lib.migrations' doesn't exist in engine
+-- Error reading data for table lib.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lib`.`migrations`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+-- Error reading structure for table lib.password_resets: #1932 - Table 'lib.password_resets' doesn't exist in engine
+-- Error reading data for table lib.password_resets: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lib`.`password_resets`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `photos`
+--
+-- Error reading structure for table lib.photos: #1932 - Table 'lib.photos' doesn't exist in engine
+-- Error reading data for table lib.photos: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lib`.`photos`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stores`
+--
+-- Error reading structure for table lib.stores: #1932 - Table 'lib.stores' doesn't exist in engine
+-- Error reading data for table lib.stores: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lib`.`stores`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table lib.users: #1932 - Table 'lib.users' doesn't exist in engine
+-- Error reading data for table lib.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lib`.`users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
